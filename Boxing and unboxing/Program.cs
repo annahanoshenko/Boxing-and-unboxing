ee51720f9d1a16fbea19047e77c2c8e2cba27c83
Anna Hanoshenko <hanoshenkoanna@gmail.com>
2024-11-28 12:56:02 +0100

    Add project files.

---
+++ Boxing and unboxing/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Boxing_and_unboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            a.GetType(); //boxing
+            
+            //object b = a;
+            //int c = (int)b; //unboxing
+
+           // decimal d = (decimal)b; InvslidCastException  
+        }
+    }
+}
